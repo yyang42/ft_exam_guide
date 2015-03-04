3d2c6bc91f9af8ea6faea2929ca50e3526950ac2
juschaef <juschaef@student.42.fr>
2015-03-04 13:35:33 +0100

    count_island: add complete version + test_map

--- exos/difficile/count_island/count_island.c
+++ exos/difficile/count_island/count_island.c
@@ -1,87 +1,7 @@
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <fcntl.h>
 
-int		ft_strlen(char *s)
-{
-	return (*s ? ft_strlen(++s) + 1 : 0);
-}
-
-void	print_map(char **map)
-{
-	char *next;
-	while (*map)
-	{	next = *(map + 1);
-		write(1, *map, ft_strlen(*map));
-		free(*map);
-		*map = next;
-	}
-	free(map);
-}
-
-void	mark_map(char **map, int j, int i, char c)
-{
-	if (!map[j])
-		return ;
-	if (map[j][i] != 'X')
-		return ;
-	map[j][i] = c;
-	mark_map(map, j - 1, i, c);
-	mark_map(map, j + 1, i, c);
-	mark_map(map, j, i - 1, c);
-	mark_map(map, j, i + 1, c);
-}
-
-void	infect_map(char **map)
-{
-	int i;
-	int j;
-	char c;
-
-	j = 0;
-	c = '0';
-	while (map[j])
-	{
-		i = 0;
-		while (map[j][i])
-		{
-			if (map[j][i] == 'X')
-			{
-				mark_map(map, j, i, c);
-				c++;
-			}
-			i++;
-		}
-		j++;
-	}
-}
-
-char	**get_map(char *s)
-{
-	char **map;
-	int i;
-	int j;
-
-	map = (char **)malloc(100000);
-	i = 0;
-	j = 0;
-	map[j] = (char *)malloc(2000);
-	while (*s)
-	{
-		map[j][i] = *s;
-		i++;
-		if (*s == '\n')
-		{
-			map[j][i] = 0;
-			i = 0;
-			j++;
-			map[j] = (char *)malloc(2000);
-		}
-		s++;
-	}
-	map[j] = NULL;
-	return (map);
-}
 
 void	ft_strcat(char *s1, char *s2)
 {
@@ -91,35 +11,170 @@ void	ft_strcat(char *s1, char *s2)
 		;
 }
 
-char	*parsing(char *s)
+char *read_map(char *map)
 {
-	char *map;
-	char *tmp;
-	char buff[1025];
-	int ret;
 	int fd;
+	int ret;
+	char *map_str;
+	char buff[10001];
 
-	map = (char *)malloc(1000000);
+	fd = open(map, O_RDONLY);
+	if (fd == -1)
+		return (NULL);
 	ret = 1;
-	fd = open(s, O_RDONLY);
-	while ((ret = read(fd, buff, 1024)))
+	map_str = malloc(1000000);
+	while ((ret = read (fd,buff, 10000)))
 	{
+		if (ret == -1)
+			return (NULL);
 		buff[ret] = 0;
-		ft_strcat(map, buff);
+		ft_strcat(map_str, buff);
 	}
-	tmp = ft_strdup(map);
-	free(map)
-	return (tmp);
+	close (fd);
+	return (map_str);
 }
 
-int		main(int ac, char **av)
+int	line_len(char *s)
+{
+	int i;
+	
+	i = 0;
+	while (s[i] && s[i] != '\n')
+		i++;
+	return (i);
+}
+
+int	count_nl(char *s)
+{
+	int count;
+	int i;
+
+	count = 0;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == '\n')
+			count++;
+		i++;
+	}
+	if (s[i - 1] != '\n')
+		return (-1);
+	return (count);
+}
+
+char **create_map(char *s)
+{
+	int len;
+	int nb_nl;
+	char **map;
+	int x;
+	int y;
+
+	len = line_len(s);
+	nb_nl = count_nl(s);
+	map = malloc(sizeof(char *) * 10000);
+	y = 0;
+	x = 0;
+	map[y] = malloc(10000);
+	while (*s)
+	{
+		map[y][x] = *s;
+		x++;
+		if (*s == '\n')
+		{
+			if (x != len + 1)
+				return (NULL);
+			map[y][x] = 0;
+			x = 0;
+			y++;
+			map[y] = malloc(100000);
+		}
+		s++;
+	}
+	map[y] = NULL;
+	if (y != nb_nl)
+		return (NULL);
+	return (map);
+}
+
+void	mark_map(char **map, int y, int x, char m)
+{
+	if (!map[y])
+		return ;
+	if (map[y][x] != 'X')
+		return ;
+	map[y][x] = m;
+	mark_map(map, y - 1, x, m);
+	mark_map(map, y + 1, x, m);
+	mark_map(map, y, x - 1, m);
+	mark_map(map, y, x + 1, m);
+}
+
+void	travel_map(char **map)
+{
+	int x;
+	int y;
+	char mark;
+
+	y = 0;
+	mark = '0';
+	while (map[y])
+	{
+		 x = 0;
+		 while (map[y][x])
+		 {
+		 	if(map[y][x] == 'X')
+			{
+				mark_map(map, y, x, mark);
+				mark++;
+			}
+			x++;
+		 }
+		 y++;
+	}
+}
+
+void	ft_putstr(char *s)
+{
+	while (*s)
+		write(1, s++, 1);
+}
+
+void	print_map(char **map)
+{
+	int y;
+
+	y = 0;
+	while (map[y])
+	{
+		ft_putstr(map[y]);
+		y++;
+	}
+}
+
+int	main(int ac, char **av)
 {
 	char *map_str;
 	char **map;
 
-	map_str = parsing(av[1]);
-	map = get_map(map_str);
-	infect_map(map);
-	print_map(map);
+	if (ac == 2)
+	{
+		map_str = read_map(av[1]);
+		if (map_str)
+		{
+			map = create_map(map_str);
+			if (map)
+			{
+				travel_map(map);
+				print_map(map);
+			}
+			else
+				write(1, "\n", 1);	
+		}
+		else
+			write(1, "\n", 1);	
+	}
+	else
+		write(1, "\n", 1);
 	return (0);
 }
