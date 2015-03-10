9e623fb03da4f0a528cbd9c5d97a16048ea111ab
yyang42 <yan.yaang@gmail.com>
2015-03-10 15:55:47 +0100

    update count island

--- exos/difficile/count_island/count_island.c
+++ exos/difficile/count_island/count_island.c
@@ -1,180 +1,172 @@
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <fcntl.h>
-
-
-void	ft_strcat(char *s1, char *s2)
-{
-	while (*s1)
-		s1++;
-	while ((*s1++ = *s2++))
-		;
-}
-
-char *read_map(char *map)
-{
-	int fd;
-	int ret;
-	char *map_str;
-	char buff[10001];
-
-	fd = open(map, O_RDONLY);
-	if (fd == -1)
-		return (NULL);
-	ret = 1;
-	map_str = malloc(1000000);
-	while ((ret = read (fd,buff, 10000)))
-	{
-		if (ret == -1)
-			return (NULL);
-		buff[ret] = 0;
-		ft_strcat(map_str, buff);
-	}
-	close (fd);
-	return (map_str);
-}
-
-int	line_len(char *s)
-{
-	int i;
-	
-	i = 0;
-	while (s[i] && s[i] != '\n')
-		i++;
-	return (i);
-}
-
-int	count_nl(char *s)
-{
-	int count;
-	int i;
-
-	count = 0;
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] == '\n')
-			count++;
-		i++;
-	}
-	if (s[i - 1] != '\n')
-		return (-1);
-	return (count);
-}
-
-char **create_map(char *s)
-{
-	int len;
-	int nb_nl;
-	char **map;
-	int x;
-	int y;
-
-	len = line_len(s);
-	nb_nl = count_nl(s);
-	map = malloc(sizeof(char *) * 10000);
-	y = 0;
-	x = 0;
-	map[y] = malloc(10000);
-	while (*s)
-	{
-		map[y][x] = *s;
-		x++;
-		if (*s == '\n')
-		{
-			if (x != len + 1)
-				return (NULL);
-			map[y][x] = 0;
-			x = 0;
-			y++;
-			map[y] = malloc(100000);
-		}
-		s++;
-	}
-	map[y] = NULL;
-	if (y != nb_nl)
-		return (NULL);
-	return (map);
-}
-
-void	mark_map(char **map, int y, int x, char m)
-{
-	if (!map[y])
-		return ;
-	if (map[y][x] != 'X')
-		return ;
-	map[y][x] = m;
-	mark_map(map, y - 1, x, m);
-	mark_map(map, y + 1, x, m);
-	mark_map(map, y, x - 1, m);
-	mark_map(map, y, x + 1, m);
-}
-
-void	travel_map(char **map)
-{
-	int x;
-	int y;
-	char mark;
-
-	y = 0;
-	mark = '0';
-	while (map[y])
-	{
-		 x = 0;
-		 while (map[y][x])
-		 {
-		 	if(map[y][x] == 'X')
-			{
-				mark_map(map, y, x, mark);
-				mark++;
-			}
-			x++;
-		 }
-		 y++;
-	}
-}
 
 void	ft_putstr(char *s)
 {
 	while (*s)
-		write(1, s++, 1);
+	{
+		write(1, s, 1);
+		s++;
+	}
 }
 
-void	print_map(char **map)
+int ft_strlen(char *s)
 {
-	int y;
+	int len;
 
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+char *read_map(char *path)
+{
+	int fd;
+	int ret;
+	char c;
+	int char_pos;
+	char *mapstr;
+
+	ret = -1;
+	mapstr = malloc(sizeof(char) * (1000 * 1000 * 10));
+	fd = open(path, O_RDONLY);
+	if (fd == -1)
+		return (NULL);
+	char_pos = 0;
+	if (!fd)
+		return (NULL);
+	while ((ret = read(fd, &c, 1)) > 0)
+	{
+		if (c != '\n' && c != 'X' && c != '.')
+			return (NULL);
+		mapstr[char_pos] = c;
+		char_pos++;
+	}
+	if (ret == -1)
+		return (NULL);
+	mapstr[char_pos] = 0;
+	if (mapstr[char_pos - 1] != '\n')
+		return (NULL);
+	return (mapstr);
+}
+
+char **build_map(char *s)
+{
+	char **map;
+	int y;
+	int x;
+	int line_len;
+
+	line_len = -1;
+	map = malloc(sizeof(char *) * (2000));
 	y = 0;
+	x = 0;
+	map[0] = malloc(sizeof(char) * (2000));
+	while (*s)
+	{
+		if (*s == '\n')
+		{
+			map[y][x] = 0;
+			if (line_len == -1)
+				line_len = ft_strlen(map[y]);
+			else if (line_len != ft_strlen(map[y]))
+				return (NULL);
+			y++;
+			map[y] = malloc(sizeof(char) * (2000));
+			x = 0;
+		}
+		else
+		{
+			map[y][x] = *s;
+			x++;
+		}
+		s++;
+	}
+	map[y] = NULL;
+	return (map);
+}
+
+void print_map(char **map)
+{
+	while (*map)
+	{
+		ft_putstr(*map);
+		ft_putstr("\n");
+		map++;	
+	}
+}
+
+
+#include <stdio.h>
+
+void infect(char **map, int y, int x, char count)
+{
+	if (y >= 0 && x >= 0
+		&& map[y]
+		&& map[y][x]
+		&& map[y][x] == 'X')
+	{
+		map[y][x] = count;
+		infect(map, y + 1, x, count);
+		infect(map, y - 1, x, count);
+		infect(map, y, x + 1, count);
+		infect(map, y, x - 1, count);
+	}
+}
+
+void travel(char **map)
+{
+	int x = 0;
+	int y = 0;
+	char count = '0';
+
 	while (map[y])
 	{
-		ft_putstr(map[y]);
+		x = 0;
+		while (map[y][x])
+		{
+			if (map[y][x] == 'X')
+			{
+				infect(map, y, x, count);
+				count++;
+			}
+			// write(1, &(map[y][x]), 1);
+			x++;
+		}
+		// write(1, "\n", 1);
 		y++;
 	}
 }
 
-int	main(int ac, char **av)
+int main(int ac, char **av)
 {
 	char *map_str;
 	char **map;
 
 	if (ac == 2)
 	{
-		map_str = read_map(av[1]);
-		if (map_str)
+		if ((map_str = read_map(av[1])))
 		{
-			map = create_map(map_str);
+			// ft_putstr("====\n");
+			// ft_putstr(map_str);
+			// ft_putstr("====\n");
+			map = build_map(map_str);
 			if (map)
 			{
-				travel_map(map);
+				travel(map);
+				// ft_putstr("====\n");
 				print_map(map);
 			}
 			else
-				write(1, "\n", 1);	
+			ft_putstr("\n");
 		}
 		else
-			write(1, "\n", 1);	
+		{
+			ft_putstr("\n");
+		}
 	}
-	else
-		write(1, "\n", 1);
+	(void)av;
 	return (0);
 }
